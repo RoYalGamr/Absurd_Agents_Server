00818622b8c19905d1fd9e9a476e36787e2ff946
Ritvik25goyal <goyalritvik9@gmail.com>
2023-08-31 00:56:38 +0530

    4 games done

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-        #     min_similarity = word_length
-        #     for word in valid_words:
-        #         similarity = sum(1 for g, w in zip(guesses, word) if g != w)
-        #         if similarity < min_similarity:
-        #             min_similarity = similarity
-        #             my_word = word
